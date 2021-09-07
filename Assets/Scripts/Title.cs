eaecc086d2d1677efd6b31217cc5b6c524113ffd
AkimotoYuhi <chiisanaaki@gmail.com>
2021-09-07 21:46:18 +0900

    タイトル画面の基本実装

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    [SerializeField] private GameObject m_title;
+    [SerializeField] private GameObject m_select;
+    private bool m_swicth = false;
+
+    private void Start()
+    {
+        m_select.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!m_swicth && Input.anyKeyDown)
+        {
+            TitleButton();
+        }
+    }
+
+    public void TitleButton()
+    {
+        m_swicth = true;
+        m_title.SetActive(false);
+        m_select.SetActive(true);
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
